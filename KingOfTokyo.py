4e6024d9e226cb5f1c8707395a0e95ec739d0071
Dustin Martin <36971079+Dusmar6@users.noreply.github.com>
2019-11-20 18:14:49 -0800

    Update KingOfTokyo.py

--- KingOfTokyo.py
+++ KingOfTokyo.py
@@ -7,27 +7,39 @@ import random
 
 class Player:
     #TODO
-    #att: name, monster, cards, energy, roll order?
+    #att: Monster (single monster object), Cards (list of Card Objects, init to empty), hearts (int, init to 10), points (int, init to 0), energy (int, init to 0), alive (bool, init to 0)
+    #object is created by passing only a monster object to constructor.
+    #basic setters and getters for all
+    #for Cards, make functions: add_card(card object), remove_card(card object). if succesful, return 1, if unsuccesful (like removing a card from an empty list), return -1
+    #for Hearts, Points, Energy: make functions that increase and decrease the amount by a given parameter (i.e add_heart(int)).
+    #   make sure Points and Energy cannot go below 0. If hearts goes below 0, set alive attribute to false
 
 class Card:
-    #TODO
-    #att: name, description, abilities, active, inactive, etc
-    #how will we apply these abilities to the game?
-    #need to implement modifiers somehow
+    def __init__(self, name, cost, card_type, ability_desc):
+        self.name = name
+        self.cost = int(cost)
+        self.card_type = card_type
+        self.ability_desc = ability_desc
+
+    def get_name(self):
+        return self.name
+
+    def get_cost(self):
+        return self.cost
+
+    def get_card_type(self):
+        return self.card_type
+
+    def get_ability_desc(self):
+        return self.ability_desc
+    
     #will need to run a check for cards in play on each turn and have a system to apply these effects
 
-class Board:
-    #TODO
-    #lists holding positions of players
-    #in tokyo, out of tokyo
     
 class Monster:
     #TODO
-    #att: name, desc.
-    
-class Energy:
-    #TODO
-    #perhaps this class is not needed as it is a simple int, could be an att for player.
+    #att: name (string), desc(string), character_sound (string) )
+    #no setters, just getters.
     
 class GameState:
     #TODO
@@ -46,12 +58,20 @@ def buy_card():
     #gives player a card
     
 def get_input(min_inp, max_inp):
-    #i need function that prompts for input and checks validitiy, 
-    #if its a bad input, print error message and ask again.
-    #requirements: between max and min
-    #if max and min are both zero, then input needs to be a lenght of at least one.
-    #return value.
-    
+    if(min_inp == 0 and max_inp == 0): 
+        user_Input = input("")
+        while(len(user_Input) < 1):
+            print("Please enter a valid input" )
+            user_Input = input("")
+    else:
+        while(user_Input < min_inp and user_Input > max_inp):
+            print("Please enter a valid input" )
+            user_Input = input("")
+    return user_Input
+
+def randomize(x):
+    return random.shuffle(x)
+
 def run_game():
     #main game logic
     
